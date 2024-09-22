5298c835b4cb32bc5f414a916a8a0ca2be7150d9
FreelancerMamun <mamunkhan16345@gmail.com>
2024-09-22 08:15:15 +0600

    delete file

--- client/src/components/PageNot.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const PageNot = () => {
-  return <div>PageNot</div>;
-};
-
-export default PageNot;
--- server/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  Email: 'tommie2@ethereal.email',
-  password: 'Nfk6sXpF46qRhW2yNm',
-};
-
-const SERVER_DOMAIN_NAME = 'http://localhost:4057';
-
-export { config };
